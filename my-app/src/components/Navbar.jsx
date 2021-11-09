c4665bd32789df5d42b0044373a3f06c418aa725
Manjil Ghale <rilleymanjil@hotmail.com>
2021-11-09 16:50:53 +0000

    + Modulles

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import Navigations from './Navigations';
-import MobileNav from './MobileNav';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
 import { Avatar, Button, Typography } from 'antd';
 import icon from'../images/cryptocurrency.png';
-=======
-import { Avatar, Typography } from 'antd';
-import icon from'../images/menu.png';
-import { DownSquareOutlined } from '@ant-design/icons';
->>>>>>> 456df153fa1f6a25da3eee055f6811c7d2777011
+import Navigations from './Navigations';
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
     return (
@@ -17,13 +11,13 @@ const Navbar = () => {
             <nav>
                 <div class="nav-container">
                     <div className="logo-container">
-                    <DownSquareOutlined style={{fontSize: 40, color:'white'}}/>
+                        <Avatar src={icon} size="medium"></Avatar>
                         <Typography.Title level={2} className="logo">
                             <Link to="/Home">Crypto Tracker</Link>
                         </Typography.Title>
-                    </div> 
-                    <Navigations />
-                    <MobileNav />
+                    </div>
+                        <Navigations />
+                        <MobileNav />
                 </div>
             </nav>
         </div>
